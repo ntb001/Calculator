2bf8c25bd3cbd143fc080a01c2decb992048792c
ntb001 <ntb001@users.noreply.github.com>
2015-03-01 15:22:09 -0500

    Add pan

--- Calculator/PlotView.swift
+++ Calculator/PlotView.swift
@@ -25,6 +25,21 @@ class PlotView: UIView {
             isOriginSet = true
         }
     }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    private func setup() {
+        let recognizer = UIPanGestureRecognizer(target: self, action: "pan:")
+        addGestureRecognizer(recognizer)
+    }
 
     override func drawRect(rect: CGRect) {
         if !isOriginSet {
@@ -32,6 +47,17 @@ class PlotView: UIView {
         }
         axesHelper.contentScaleFactor = contentScaleFactor
         axesHelper.drawAxesInRect(rect, origin: plotOrigin, pointsPerUnit: scale)
+    }
+    
+    func pan(gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .Changed: fallthrough
+        case .Ended:
+            let translation = gesture.translationInView(self)
+            plotOrigin = CGPoint(x: plotOrigin.x + translation.x, y: plotOrigin.y + translation.y)
+            gesture.setTranslation(CGPointZero, inView: self)
+        default: break
+        }
     }
 
 }
